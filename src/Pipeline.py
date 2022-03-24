a9bd681b1a256836d2828d641894a45ea296b055
poli-cz <Polisensky.john@gmail.com>
2022-03-24 20:12:12 +0100

    Example: cleanup

--- src/Pipeline.py
+++ src/Pipeline.py
@@ -10,13 +10,19 @@ from Lex import Net
 import tensorflow as tf
 
 
-print("[Log]: Starting modules...")
+
 
 print("[Prompt]: Enter hostname:")
 hostname = input()
 hostname = str(hostname)
+
+
+
+# Data parsing module
 domain = Base_parser(hostname)
 
+
+# Loading values from different categories
 print("[Info]: Loading values...")
 domain.load_dns_data()
 domain.load_geo_info()
@@ -24,6 +30,7 @@ domain.load_whois_data()
 domain.load_ssl_data()
     
 
+# Retrieving values
 dns_data = domain.get_dns()
 geo_data = domain.get_geo_data()
 whois_data = domain.get_whois_data()
@@ -32,19 +39,24 @@ ssl_data = domain.get_ssl_data()
 
 
 if Lex.is_empty(dns_data) and Lex.is_empty(geo_data) and Lex.is_empty(whois_data):
-        print("[Error]: Failed to harvers domain data")
-        print("[Info]: Switching to lexical analysis AI")
+        print("[Error]: Failed to harvers large amount of data")
+        exit(1)
         model = tf.saved_model.load('./domain_bigrams-furt-2020-11-07T11_09_21')
         input()
 
 
 
+
+# Get data together to some structure
 domain_data = {"name": hostname, "dns_data": dns_data, "geo_data": geo_data, "whois_data": whois_data, "ssl_data": ssl_data}
 
-print("[Info]: Values loaded, starting AI")
 
+
+# Module for data evaluation and prediction
 prediction = Lex.process_data(domain_data)
-print("[Info]: ", prediction)
+
+
+#print("[Info]: ", prediction)
 prediction = float(prediction)
 
 print("----------------------------------------------------------------")
